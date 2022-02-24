94ed89e499a6e5606fd21a5e137f8f88eb4019e6
TheNeck94 <76504387+TheNeck94@users.noreply.github.com>
2022-02-24 09:15:46 -0400

    Add files via upload

---
+++ WalshLab5/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace WalshLab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblPrincipal = new System.Windows.Forms.Label();
+            this.txtPrincipal = new System.Windows.Forms.TextBox();
+            this.lblRate = new System.Windows.Forms.Label();
+            this.lblRatePercent = new System.Windows.Forms.Label();
+            this.lblYears = new System.Windows.Forms.Label();
+            this.txtYears = new System.Windows.Forms.TextBox();
+            this.lblMonthlyMortgage = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.pboxHouse = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxHouse)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(12, 213);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(102, 45);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(121, 213);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(98, 44);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(226, 213);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(104, 44);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblPrincipal
+            // 
+            this.lblPrincipal.AutoSize = true;
+            this.lblPrincipal.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrincipal.Location = new System.Drawing.Point(8, 35);
+            this.lblPrincipal.Name = "lblPrincipal";
+            this.lblPrincipal.Size = new System.Drawing.Size(73, 19);
+            this.lblPrincipal.TabIndex = 3;
+            this.lblPrincipal.Text = "Principal:";
+            // 
+            // txtPrincipal
+            // 
+            this.txtPrincipal.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrincipal.Location = new System.Drawing.Point(98, 35);
+            this.txtPrincipal.Name = "txtPrincipal";
+            this.txtPrincipal.Size = new System.Drawing.Size(125, 27);
+            this.txtPrincipal.TabIndex = 1;
+            // 
+            // lblRate
+            // 
+            this.lblRate.AutoSize = true;
+            this.lblRate.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRate.Location = new System.Drawing.Point(37, 75);
+            this.lblRate.Name = "lblRate";
+            this.lblRate.Size = new System.Drawing.Size(44, 19);
+            this.lblRate.TabIndex = 5;
+            this.lblRate.Text = "Rate:";
+            // 
+            // lblRatePercent
+            // 
+            this.lblRatePercent.AutoSize = true;
+            this.lblRatePercent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRatePercent.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRatePercent.Location = new System.Drawing.Point(98, 77);
+            this.lblRatePercent.Name = "lblRatePercent";
+            this.lblRatePercent.Size = new System.Drawing.Size(51, 21);
+            this.lblRatePercent.TabIndex = 6;
+            this.lblRatePercent.Text = "2.99%";
+            // 
+            // lblYears
+            // 
+            this.lblYears.AutoSize = true;
+            this.lblYears.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYears.Location = new System.Drawing.Point(33, 115);
+            this.lblYears.Name = "lblYears";
+            this.lblYears.Size = new System.Drawing.Size(48, 19);
+            this.lblYears.TabIndex = 7;
+            this.lblYears.Text = "Years:";
+            // 
+            // txtYears
+            // 
+            this.txtYears.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtYears.Location = new System.Drawing.Point(98, 113);
+            this.txtYears.Name = "txtYears";
+            this.txtYears.Size = new System.Drawing.Size(121, 27);
+            this.txtYears.TabIndex = 2;
+            // 
+            // lblMonthlyMortgage
+            // 
+            this.lblMonthlyMortgage.AutoSize = true;
+            this.lblMonthlyMortgage.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMonthlyMortgage.Location = new System.Drawing.Point(8, 157);
+            this.lblMonthlyMortgage.Name = "lblMonthlyMortgage";
+            this.lblMonthlyMortgage.Size = new System.Drawing.Size(143, 19);
+            this.lblMonthlyMortgage.TabIndex = 9;
+            this.lblMonthlyMortgage.Text = "Monthly Mortgage:";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutput.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(166, 155);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(103, 21);
+            this.lblOutput.TabIndex = 10;
+            this.lblOutput.Text = "                       ";
+            // 
+            // pboxHouse
+            // 
+            this.pboxHouse.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pboxHouse.BackgroundImage")));
+            this.pboxHouse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pboxHouse.Location = new System.Drawing.Point(323, 12);
+            this.pboxHouse.Name = "pboxHouse";
+            this.pboxHouse.Size = new System.Drawing.Size(202, 191);
+            this.pboxHouse.TabIndex = 11;
+            this.pboxHouse.TabStop = false;
+            this.pboxHouse.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(563, 270);
+            this.Controls.Add(this.pboxHouse);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblMonthlyMortgage);
+            this.Controls.Add(this.txtYears);
+            this.Controls.Add(this.lblYears);
+            this.Controls.Add(this.lblRatePercent);
+            this.Controls.Add(this.lblRate);
+            this.Controls.Add(this.txtPrincipal);
+            this.Controls.Add(this.lblPrincipal);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalculate);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximumSize = new System.Drawing.Size(580, 310);
+            this.MinimumSize = new System.Drawing.Size(579, 309);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mortgage Calculator by Brady";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pboxHouse)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblPrincipal;
+        private System.Windows.Forms.TextBox txtPrincipal;
+        private System.Windows.Forms.Label lblRate;
+        private System.Windows.Forms.Label lblRatePercent;
+        private System.Windows.Forms.Label lblYears;
+        private System.Windows.Forms.TextBox txtYears;
+        private System.Windows.Forms.Label lblMonthlyMortgage;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.PictureBox pboxHouse;
+    }
+}
+
---
+++ WalshLab5/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/// <summary>
+/// ITPA:1B -WalshLab5 (TryMath) DueDate: Oct 21, 2021 8:30 AM
+/// Description: This program takes in two user input values, converts them to doubles and calculates output using a pre-determined formula (Mortgage Rate)
+/// </summary>
+namespace WalshLab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Establish RATE as constant double with the value of 0.0299, this represents the mortgage rate
+        const double RATE = 0.0299;
+        //Reset button Clears text fields and outputs, hides picture and sets focus to the first text input
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtPrincipal.Clear();
+            txtPrincipal.Focus();
+            txtYears.Clear();
+            lblOutput.Text = " ";
+            pboxHouse.Visible = false;
+
+        }
+        //Exit Button closes the program and any processes
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        // on form load, the Rate text box is assigned the value of the RATE var
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblRate.Text = RATE.ToString();
+
+        }
+        // on click, if statement check validates the input, then calculates the output and displays a formatted string
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(txtPrincipal.Text) < 5000)
+            { MessageBox.Show("Invalid Principal amount, try again"); }
+            else if (Convert.ToInt32(txtYears.Text) > 30)
+            { MessageBox.Show("Invalid Years amount, try again"); }
+            else
+            {
+                try
+                {
+                    double N = (Convert.ToDouble(txtYears.Text) / 12);
+                    double R = RATE;
+                    double V = (1 / R);
+                    double Output = (((Convert.ToDouble(txtPrincipal.Text) * Math.Pow((R * V), (N)))) / Math.Pow(V, N)) - 1;
+                    double Result = (Math.Round(Output, 2));
+                    lblOutput.Text = Result.ToString("C");
+                    pboxHouse.Visible = true;
+                }
+                catch
+                {
+                    string message = "Incorrect ionput encountered";
+                    string title = "Error";
+                    MessageBoxButtons buttons = MessageBoxButtons.OK;
+                    MessageBox.Show(message, title, buttons);
+                }
+            }
+        }
+    }
+}
